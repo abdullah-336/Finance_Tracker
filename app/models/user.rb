3f818fb0f5bdd61541bbd440f15eb25a0e2a5eb4
abdullah-336 <abdullah.zafar@devden.io>
2021-08-20 00:22:07 +0500

    Friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_stockings
   has_many :stocks, through: :user_stockings
+  has_many :friendships
+  has_many :friends, through: :friendships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -34,37 +36,39 @@ class User < ApplicationRecord
   
   
   
-  # def not_friends_with?(friend_id)
-  #   friendships.where(friend_id: friend_id).count < 1
-  # end
+  def not_friends_with?(id_of_friend)
+    !self.friends.where(id: id_of_friend).exists?
+  end
   
-  # def except_current_user(users)
-  #   users.reject { |user| user.id == self.id }
-  # end
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
+  end
   
-  # def self.search(param)
-  #   return User.none if param.blank?
+  def self.search(param)
+    return User.none if param.blank?
     
-  #   param.strip!
-  #   param.downcase!
-  #   (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
-  # end
+    param.strip!
+    param.downcase!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nill unless to_send_back
+    to_send_back
+  end
   
-  # def self.first_name_matches(param)
-  #   matches('first_name', param)
-  # end
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
   
-  # def self.last_name_matches(param)
-  #   matches('last_name', param)
-  # end
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
   
-  # def self.email_matches(param)
-  #   matches('email', param)
-  # end
+  def self.email_matches(param)
+    matches('email', param)
+  end
   
-  # def self.matches(field_name, param)
-  #   where("lower(#{field_name}) like ?", "%#{param}%")
-  # end
+  def self.matches(field_name, param)
+    where("lower(#{field_name}) like ?", "%#{param}%")
+  end
 end
   
 
